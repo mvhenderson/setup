c4b6a8cb6290df46a07ad4dcee6b04cb8d8ad960
Michael Henderson <mvhenderson@tds.net>
2013-11-25 21:58:57 -0600

    osx: desktop, screen saver, dock

--- osx.sh
+++ osx.sh
@@ -16,49 +16,39 @@
 # Utility Functions
 #############################################################################
 
-# Headers
-function h1()  { echo "\n\033[1m\033[4m$@\033[0m"; }
-function h2()  { echo "\n \033[1m$@\033[0m"; }
-
-# Log success/failure
-#		$1 - exit code of command
-#		$2 - message to print
-function log() {
-	local exit=$1
-	if [[ $exit -eq 0 ]];	then
-		echo "  \033[32m✔\033[0m $2"
-	else
-		echo "  \033[1;31m✖\033[0m $2"
-	fi
-}
-
-# Change preference showing both before/after values
-#		$1 - domain
-#		$2 - key
-#		$3 - type
-#		$4 - value
-function set_default () {
-	local before=`/usr/bin/defaults read $1 $2 2>/dev/null`
-	if [[ "$before" == "$4" ]]; then
-		echo "    \033[2m$2\033[0m"
-	else
-		/usr/bin/defaults write "$@" #quote so bash doesn't expand string values with spaces
-		log $? "$2 \033[2m$before ➜ $4\033[0m"
-	fi
-}
-
-# Change appearance preferance via AppleScript
-#		$1 property name
-#		$2 value
-function set_appearance () {
-	local before=$(/usr/bin/osascript -e 'tell application "System Events" to tell appearance preferences to get '"$1" 2>/dev/null)
-	if [[ "$before" == "$2" ]]; then
-		echo "    \033[2m$1\033[0m"
-	else
-		/usr/bin/osascript -e 'tell application "System Events" to tell appearance preferences to set '"$1"' to '"$2"
-		log $? "$1 \033[2m$before ➜ $2\033[0m"
-	fi
-}
+# h1(), h2(), h3() : Headers
+	function h1()  { echo "\n\033[1m\033[4m$@\033[0m"; }
+	function h2()  { echo "\n \033[1m$@\033[0m"; }
+	function h3()  { echo "\n  $@"; }
+# log(exit, message) : Log success/failure
+	function log() {
+		local exit=$1
+		if [[ $exit -eq 0 ]];	then
+			echo "  \033[32m✔\033[0m $2"
+		else
+			echo "  \033[1;31m✖\033[0m $2"
+		fi
+	}
+# set_default(domain, key, type, value) : Change preference showing both before/after values
+	function set_default () {
+		local before=`/usr/bin/defaults read $1 $2 2>/dev/null`
+		if [[ "$before" == "$4" ]]; then
+			echo "    \033[2m$2\033[0m"
+		else
+			/usr/bin/defaults write "$@" #quote so bash doesn't expand string values with spaces
+			log $? "$2 \033[2m$before ➜ $4\033[0m"
+		fi
+	}
+# set_appearance(property, value) : Change appearance preferance via AppleScript
+	function set_appearance () {
+		local before=$(/usr/bin/osascript -e 'tell application "System Events" to tell appearance preferences to get '"$1" 2>/dev/null)
+		if [[ "$before" == "$2" ]]; then
+			echo "    \033[2m$1\033[0m"
+		else
+			/usr/bin/osascript -e 'tell application "System Events" to tell appearance preferences to set '"$1"' to '"$2"
+			log $? "$1 \033[2m$before ➜ $2\033[0m"
+		fi
+	}
 
 #############################################################################
 # System Preferences
@@ -117,7 +107,64 @@ h1 "System Preferences"
 			# font smoothing style:automatic, 
 			# smooth scrolling:true, 
 	h2 "Desktop & Screen Saver"
-	# h2 "Dock"
+		h3 "Desktop"
+			# NOTE: not much here intentionally since I use images/iPhoto backgrounds
+			#				See ptb's repo for example of solid color background
+
+			# Translucent menu bar
+				# 0 - off
+				# 1 - on (default)
+				set_default -g 'AppleEnableMenuBarTransparency' -int 1
+		h3 "Screen Saver"
+			# Screen Saver
+				# NOTE: screen saver turned off, let power mgmt deal with this
+				/usr/bin/defaults -currentHost write com.apple.screensaver '{ idleTime = 0; moduleDict = { moduleName = "Computer Name"; path = "/System/Library/Frameworks/ScreenSaver.framework/Resources/Computer Name.saver"; type = 0; }; }';
+				log $? "turned off"
+		h3 "Hot Corners"
+			# Bottom Left: sleep
+				# TODO: document possible values
+		    set_default com.apple.dock 'wvous-bl-corner' -int 10
+		    set_default com.apple.dock 'wvous-bl-modifier' -int 0
+	h2 "Dock"
+		# Size
+			# small (16) - large (128)
+			# TODO: double check the default
+			set_default com.apple.dock 'tilesize' -int 32
+		# Magnification
+			# 0 - off
+			# 1 - on (default)
+			set_default com.apple.dock 'magnification' -int 0
+			# min (16) - max (128)
+			# TODO: what is the default
+			set_default com.apple.dock 'largesize' -int 64
+		# Position on screen
+			# left
+			# bottom (default)
+			# right
+			set_default com.apple.dock 'orientation' -string 'left'
+		# Minimize windows using
+			# genie - Genie effect (default)
+			# scale - Scale effect
+			set_default com.apple.dock 'mineffect' -string 'genie'
+		# Double-click window title bar to minimize
+			# TODO: doesn't appear to be in com.apple.dock prefs
+		# Minimize windows into application icon
+			# 0 - no (default)
+			# 1 - yes
+			set_default com.apple.dock 'minimize-to-application' -int 0
+		# Animate opening applications
+			# 0 - no
+			# 1 - yes (default)
+			set_default com.apple.dock 'launchanim' -int 1
+		# Automatically hide and show the Dock
+			# 0 - no (default)
+			# 1 - yes
+			set_default com.apple.dock 'autohide' -int 1
+		# Show indicator lights for open applications
+			# 0 - no
+			# 1 - yes (default)
+			set_default com.apple.dock 'show-process-indicators' -int 1
+
 	# h2 "Misson Control"
 	# h2 "Language & Region"
 	# h2 "Security & Privacy"
